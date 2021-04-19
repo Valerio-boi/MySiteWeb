1f42411c543b1da194b1d3c0d8dbfe5a6ecb1b9c
Valerio <boivalerio@gmail.com>
2021-04-19 21:06:43 +0200

    avanzamento

--- mysiteweb/src/components/InitialDivBackground.js
+++ mysiteweb/src/components/InitialDivBackground.js
@@ -1,17 +1,56 @@
 import React from 'react'
 import BackgroundInitial from '../background/sfondoIniziale.jpg'
+import LinkLogo from "./LinkLogo"
 import NavBar from './navBar'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function backgroundDiv() {
 
+
+
+
     return (
-        <div style={{
-            backgroundImage: `url(${BackgroundInitial})`, height: '100vh', backgroundPosition: 'center',
-            backgroundRepeat: 'no-repeat',
-            backgroundSize: 'cover'
-        }}>
-            <NavBar/>
-        </div>
+        <>
+            <div style={{
+                backgroundImage: `url(${BackgroundInitial})`, height: '100vh', backgroundPosition: 'center',
+                backgroundRepeat: 'no-repeat',
+                backgroundSize: 'cover',
+                textAlign: 'center'
+            }}>
+                <NavBar />
+                <h1 style={{ marginTop: '30px' }}>Valerio boi</h1>
+
+                <br></br>
+
+                <p>
+
+                    Sviluppatore informatico presso Exolab S.R.L, in ambito Java EE, consulente Sogei.
+                Prima di essere un lavoro è una grande passione che porto avanti giorno dopo giorno con detizione e impegno.<br /><br />
+
+                “The best programs are written so that computing machines
+                can perform them quickly and so that human beings can <br />
+                understand them clearly. A programmer is ideally an essayist
+                who works with traditional aesthetic and literary forms as well <br />
+                as mathematical concepts, to communicate the way that an
+                algorithm works and to convince a reader that the results will be correct.” <br />
+                ― Donald E. Knuth
+
+
+            </p>
+
+                <div style={{
+                    position: 'fixed',
+                    left: '50%',
+                    bottom: '-30px',
+                    transform: 'translate(-50%, -50%)',
+                    margin: '0 auto'
+                }}>
+
+                    <LinkLogo />
+
+                </div>
+            </div>
+        </>
     );
 
 
---
+++ mysiteweb/src/components/LinkLogo.js
@@ -0,0 +1,80 @@
+import curriculum from "../img/curriculum.png";
+import qrCode from "../img/qrCode.jpg";
+import github from "../img/octocat.png";
+import linkedin from "../img/linkedin.png";
+import React, { useState } from "react";
+import pdf from '../pdf/CvValerioBoi.pdf'
+
+
+export default function LinkLogo(props) {
+  const [hovered, setHovered] = useState(false);
+  const [hoveredCv, setHoveredCv] = useState(false);
+  const [hoveredGit, setHoveredGit] = useState(false);
+  const [hoveredLk, setHoveredCvLk] = useState(false);
+
+  return (
+    <>
+      <a href={pdf} download>
+        <img
+          src={curriculum}
+          alt="curriculum"
+          width={props.size < 1168 ? "70px" :  "120px"}
+          onMouseOver={() => setHoveredCv(true)}
+          onMouseLeave={() => setHoveredCv(false)}
+          style={{
+            paddingRight: "18px",
+            transform: `${hoveredCv ? "scale(1.2,1.2)" : "scale(1,1)"}`,
+            transition: `${hoveredCv ? "0.5s" : "0.5s;"}`,
+          }}
+        />
+      </a>
+
+      <img
+        src={qrCode}
+        alt="qrCode"
+        width={props.size < 1168 ? "70px" :  "120px"}
+        onMouseOver={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
+        style={{
+          paddingRight: "18px",
+          transform: `${hovered ? "scale(1.2,1.2)" : "scale(1,1)"}`,
+          transition: `${hovered ? "0.5s" : "0.5s;"}`,
+        }}
+      />
+
+      <a href="https://github.com/Valerio-boi" target="_blank">
+        <img
+          src={github}
+          alt="qrCode"
+          width={props.size < 1168 ? "90px" :  "150px"}
+          onMouseOver={() => setHoveredGit(true)}
+          onMouseLeave={() => setHoveredGit(false)}
+          style={{
+            paddingRight: "18px",
+            transform: `${hoveredGit ? "scale(1.2,1.2)" : "scale(1,1)"}`,
+            transition: `${hoveredGit ? "0.5s" : "0.5s;"}`,
+          }}
+        />
+      </a>
+
+      <a
+        href="https://www.linkedin.com/in/valerio-boi-7a55341a0/"
+        target="_blank"
+      >
+        <img
+          src={linkedin}
+          alt="linkedin"
+          width={props.size < 1168 ? "60px" :  "120px"}
+          onMouseOver={() => setHoveredCvLk(true)}
+          onMouseLeave={() => setHoveredCvLk(false)}
+          style={{
+            addingRight: "18px",
+            transform: `${hoveredLk ? "scale(1.2,1.2)" : "scale(1,1)"}`,
+            transition: `${hoveredLk ? "0.5s" : "0.5s;"}`,
+            
+          }}
+        />
+      </a>
+    </>
+  );
+}
