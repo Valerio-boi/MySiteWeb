d163ab1fda589038097cd5710615f657765ebb9c
Valerio boi <v.boi@exolab.it>
2021-04-26 11:22:17 +0200

    icone

---
+++ mysiteweb/src/components/divProject.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function divProject() {
+  return (
+    <div
+      style={{
+        backgroundColor: "black",
+        height: "100vh",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        textAlign: "center",
+      }}
+    ></div>
+  );
+}
